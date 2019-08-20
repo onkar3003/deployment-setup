b81ab915338459626dde2b8ce84e5e66624a8463
OnkarRB303 <om.onkara@gmail.com>
2019-08-20 13:21:30 +0530

    created the express server and default routes

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const bodyParser = require("body-Parser");
+
+const app = express();
+const port = process.env.PORT|| 3000
+app.use(bodyParser());
+app.get("/", (req, res) => {
+  res.send({ messgae: "welcome to weather services center" });
+});
+app.listen(port, () => {
+  console.log(`app started listing on port ${port}`);
+});
