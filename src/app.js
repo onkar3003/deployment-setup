6c72cd1f454acfcb45734be676ba21e673e2f076
OnkarRB303 <om.onkara@gmail.com>
2019-08-20 13:41:12 +0530

    renoved use of body parser

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-Parser");
 
 const app = express();
 const port = process.env.PORT|| 3000
-app.use(bodyParser());
+// app.use(bodyParser());
 app.get("/", (req, res) => {
   res.send({ messgae: "welcome to weather services center" });
 });
